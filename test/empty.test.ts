86b11cf7bdd575588e804b4ef54afdad3c91d718
Roger Kok <roger@interactive-development.nl>
2022-10-13 20:05:27 +0200

    Disable boilerplate tests.

--- test/empty.test.ts
+++ test/empty.test.ts
@@ -1,5 +1,5 @@
 describe('empty test suite', () => {
-  test('runs', () => {
-    expect(true).toBe(true);
-  });
+    test('runs', () => {
+        expect(true).toBe(true);
+    });
 });
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,13 @@
+/*
 import { add, multiply } from '../';
 
 describe('math functions', () => {
-  test('adds two numbers', () => {
-    expect(add(42, 3)).toBe(45);
-  });
+    test('adds two numbers', () => {
+        expect(add(42, 3)).toBe(45);
+    });
 
-  test('multiplies three numbers', () => {
-    expect(multiply(1, 2, 3)).toBe(6);
-  });
+    test('multiplies three numbers', () => {
+        expect(multiply(1, 2, 3)).toBe(6);
+    });
 });
+*/
