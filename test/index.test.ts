6cc33daab433603e988775ab70d6c00c131c5d6e
Roger Kok <roger@interactive-development.nl>
2022-10-14 08:19:41 +0200

    Fix some test errors.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,5 @@
-/*
-import { add, multiply } from '../';
-
-describe('math functions', () => {
-    test('adds two numbers', () => {
-        expect(add(42, 3)).toBe(45);
-    });
-
-    test('multiplies three numbers', () => {
-        expect(multiply(1, 2, 3)).toBe(6);
+describe('some day there will be an actual test', () => {
+    test('nothing happens', () => {
+        expect(1).toBe(1);
     });
 });
-*/
